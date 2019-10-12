a72392c90893251e1f89eabeb5b70ab8edb6f379
polmina <41476151+polmina@users.noreply.github.com>
2019-10-12 05:08:37 +0200

    flight crush room entering (partially) done

--- app/src/main/java/com/pol/happyflight/GamesFragments/FlightCrush.java
+++ app/src/main/java/com/pol/happyflight/GamesFragments/FlightCrush.java
@@ -1,18 +1,62 @@
 package com.pol.happyflight.GamesFragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.pol.happyflight.Classes.Room;
 import com.pol.happyflight.R;
 
 public class FlightCrush  extends Fragment {
-
+    String id = "Flight_crash";
+    FirebaseFirestore db;
+    String TAG = "FLIGHT";
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        db = FirebaseFirestore.getInstance();
+        searchForRoom();
         return inflater.inflate(R.layout.flight_crush, container, false);
+
+
+    }
+    private String searchForRoom(){
+        Log.d(TAG, "LEGO");
+            db.collection(id)
+                    .whereEqualTo("En curs", false)
+
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    String id = document.getId();
+                                    boolean enCurs =  Boolean.valueOf(document.get("En curs").toString());
+                                    int maxJug = Integer.parseInt(document.get("Max jug").toString());
+                                    int minJug = Integer.parseInt(document.get("Min jug").toString());
+                                    Room room = new Room( id,enCurs ,maxJug ,minJug  );
+                                    joinRoom(room);
+                                    break;
+
+                                }
+                            } else {
+                                Log.d(TAG, "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
+        return "";
+    }
+    private void joinRoom(Room room){
+        Log.d(TAG, "JOINED ROOM " + room.getId());
     }
 }
