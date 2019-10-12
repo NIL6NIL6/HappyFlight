fc5be34748e6208968022c369c8781122c7b1926
polmina <41476151+polmina@users.noreply.github.com>
2019-10-12 03:21:44 +0200

    games list

--- app/src/main/java/com/pol/happyflight/MainActivity.java
+++ app/src/main/java/com/pol/happyflight/MainActivity.java
@@ -3,6 +3,7 @@ package com.pol.happyflight;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Intent intent = new Intent(MainActivity.this, RoomsList.class);
         startActivity(intent);
+
+
     }
 }
--- app/src/main/java/com/pol/happyflight/RoomsList.java
+++ app/src/main/java/com/pol/happyflight/RoomsList.java
@@ -1,13 +1,48 @@
 package com.pol.happyflight;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class RoomsList extends AppCompatActivity {
-
+    LinearLayout gamesList;
+    String TAG = "ROOMS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms_list);
+        gamesList = findViewById(R.id.gamesList);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("games")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String name = document.get("name").toString();
+                                Button but = new Button(RoomsList.this);
+                                but.setText(name);
+                                gamesList.addView(but);
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
