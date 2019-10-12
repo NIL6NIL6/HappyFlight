4b9fda171fb4a5185ef5a65f46ae5652cbca8027
zemone <antoni.rambla.moiset@gmail.com>
2019-10-13 00:47:10 +0200

    fixed

--- app/src/main/java/com/pol/happyflight/GamesFragments/FlightCrush.java
+++ app/src/main/java/com/pol/happyflight/GamesFragments/FlightCrush.java
@@ -3,9 +3,11 @@ package com.pol.happyflight.GamesFragments;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.util.Pair;
@@ -13,6 +15,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -41,6 +45,7 @@ public class FlightCrush  extends Fragment {
     boolean gameHost = false;
     List<Object> users;
     View view;
+    String address;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -48,7 +53,7 @@ public class FlightCrush  extends Fragment {
         db = FirebaseFirestore.getInstance();
         WifiManager manager = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = manager.getConnectionInfo();
-        final String address = info.getMacAddress();
+        address = info.getMacAddress();
         final CollectionReference gameStat = db.collection("Flight_crash")
                 .document(GameRoom.roomId).collection("GameStat");
         gameStat.document("Users")
@@ -68,10 +73,10 @@ public class FlightCrush  extends Fragment {
                         }
                     }
                 });
-        View v = inflater.inflate(R.layout.flight_crush, container, false);
-        defineButtonClicks(v);
+        defineButtonClicks(view);
         final DocumentReference docRef = gameStat.document("Status");
         docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onEvent(@Nullable DocumentSnapshot snapshot,
                                 @Nullable FirebaseFirestoreException e) {
@@ -89,33 +94,45 @@ public class FlightCrush  extends Fragment {
                     if (gameHost){
                         Map current = (Map) snapshot.get("Current");
                         Map destination = (Map) snapshot.get("Destination");
+                        HashMap<String,Object> dummy = new HashMap<>();
                         Log.w(TAG, "Mapped");
                         if (current.keySet().size() == destination.keySet().size()){
-                            //HashMap<String,Float> intersections = theAlgorithm(current,destination);
+                            HashMap<String,Float> intersections = new HashMap<>();//theAlgorithm(current,destination);
                             //Primer borrem els destination aixi evitem entrar mes cops en aquesta part(per si es crida al listener cada cop que fem un canvi)
                             Map copy = destination;
-                            HashMap<String,Object> dummy = new HashMap<>();
+                            dummy.put("Ready",false);
                             dummy.put("Destination",new HashMap<>());
                             Log.w(TAG, "Pre-update");
                             docRef.set(dummy, SetOptions.merge());
                             Log.w(TAG, "Post-update");
                             HashMap<String,Boolean> deceased =  (HashMap<String,Boolean>) snapshot.get("Deceased");
-                            /*for (String s : intersections.keySet()){
+                            for (String s : intersections.keySet()){
                                 deceased.put(s,true);
-                            }*/
+                            }
                             dummy = new HashMap<>();
                             dummy.put("Deceased",deceased);
+                            Log.w(TAG, "Calcul RIP");
                             docRef.set(dummy,SetOptions.merge());
-                            HashMap<String,Integer> newCurrent = new HashMap<>();
+                            HashMap<String,Long> newCurrent = new HashMap<>();
                             //Fer per aqui les animacions de rip
-                            /*for (Object o : current.keySet()){
+                            for (Object o : current.keySet()){
                                 if (intersections.containsKey(o.toString()))continue;
-                                newCurrent.put(o.toString(),(Integer)current.get(o));
+                                newCurrent.put(o.toString(),(Long)copy.get(o));
+                            }
+                            HashMap<String,ArrayList<Boolean>> newVisited = new HashMap<>();
+                            HashMap<String,ArrayList<Boolean>> visited = (HashMap<String,ArrayList<Boolean>>) snapshot.get("UserVisited");
+                            for (String s : newCurrent.keySet()){
+                                ArrayList<Boolean> aux = visited.get(s);
+                                Log.w(TAG, s + Math.toIntExact(newCurrent.get(s)));
+                                aux.set(Math.toIntExact(newCurrent.get(s)),true);
+                                newVisited.put(s,aux);
                             }
                             dummy = new HashMap<>();
                             dummy.put("Current",newCurrent);
-                            dummy.put("Ready", false);*/
+                            dummy.put("UserVisited",newVisited);
+                            dummy.put("Ready", true);
                             docRef.set(dummy,SetOptions.merge());
+                            Log.w(TAG, "Fin");
                         }
                     }
                 } else {
@@ -123,7 +140,7 @@ public class FlightCrush  extends Fragment {
                 }
             }
         });
-        return v;
+        return view;
     }
 
     private void initializeGame(final CollectionReference gameStat) {
@@ -166,6 +183,7 @@ public class FlightCrush  extends Fragment {
                             }
                             data.put("UserVisited",visited);
                             data.put("Current",current);
+                            data.put("Ready",true);
                             placeInfo(gameStat,data);
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
@@ -196,7 +214,8 @@ public class FlightCrush  extends Fragment {
         for (int i = 0; i < 10; i++) {
             String buttonID = "destination".concat(Integer.toString(i+1));
             int resID = getResources().getIdentifier(buttonID, "id", "com.pol.happyflight");
-            Button button = (Button)v.findViewById(resID);
+            Log.w(TAG, resID + "");
+            ImageButton button = (ImageButton)v.findViewById(resID);
             final int idNum = i;
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -207,24 +226,49 @@ public class FlightCrush  extends Fragment {
         }
     }
 
-    private void onButtonClick(View view, int i) {
-        if (isPositionAvailable(i))
-            goToPosition(i);
-        else
-            incorrectPosition(view);
-    }
-
-    private boolean isPositionAvailable(int i) {
-        ArrayList<Boolean> positionsAvailability = getPositionsAvailability();
-        return !(positionsAvailability.get(i));
+    private void onButtonClick(final View view, final int i) {
+        db.collection("Flight_crash")
+                .document(GameRoom.roomId).collection("GameStat").document("Status")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (!(((HashMap<String,ArrayList<Boolean>>) document.get("UserVisited")).get(address)).get(i)) goToPosition(i);
+                            else incorrectPosition(view);
+                        }
+                    }
+                });
     }
 
     private void incorrectPosition(View view) {
-
+        Toast.makeText(view.getContext(), "Already been visited" , Toast.LENGTH_SHORT).show();
     }
 
-    private void goToPosition(int pos) {
-        //FIREBASE CODE HERE
+    private void goToPosition(final int pos) {
+        Log.w(TAG,"Change Pos " + pos);
+        final DocumentReference docRef =
+                db.collection("Flight_crash")
+                        .document(GameRoom.roomId).collection("GameStat").document("Status");
+                docRef.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if ((Boolean)document.get("Ready")){
+                                HashMap<String,Long> newPos = new HashMap<>();
+                                newPos = (HashMap<String,Long>) document.get("Destination");
+                                newPos.put(address, (long) pos);
+                                HashMap<String,Object> data = new HashMap<>();
+                                data.put("Destination",newPos);
+                                Log.w(TAG,"Supposed set");
+                                docRef.set(data,SetOptions.merge());
+                            }
+                        }
+                    }
+                });
     }
 
     private int getCurrentPosition() {
@@ -235,15 +279,8 @@ public class FlightCrush  extends Fragment {
         return pos;
     }
 
-    private ArrayList<Boolean> getPositionsAvailability() {
-        ArrayList<Boolean> positionsAvailability = new ArrayList<Boolean>();
-
-        //FIREBASE CODE HERE
-
-        return positionsAvailability;
-    }
-
-    /*private HashMap<String, Pair<Float, Float>> theAlgorithm(Map<String, Integer> currents, Map<String, Integer> destinations) {
+    /*
+    private HashMap<String, Pair<Float, Float>> theAlgorithm(Map<String, Integer> currents, Map<String, Integer> destinations) {
         HashMap<String, Pair<Float, Float>> deceased = new HashMap<String, Pair<Float, Float>>();
         HashMap<String, Pair<Float, Float>> lines;
 
