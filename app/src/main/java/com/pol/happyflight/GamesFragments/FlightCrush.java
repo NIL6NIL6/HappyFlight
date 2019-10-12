74ed09a56f7311358b1233b88422757c3d31863b
polmina <41476151+polmina@users.noreply.github.com>
2019-10-12 04:17:46 +0200

    room click event added

---
+++ app/src/main/java/com/pol/happyflight/GamesFragments/FlightCrush.java
@@ -0,0 +1,8 @@
+package com.pol.happyflight.GamesFragments;
+
+import android.support.v4.app.Fragment;
+
+public class FlightCrush  extends Fragment {
+
+    
+}
--- app/src/main/java/com/pol/happyflight/RoomsList.java
+++ app/src/main/java/com/pol/happyflight/RoomsList.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -33,10 +34,12 @@ public class RoomsList extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
+                            gamesList.removeAllViews();
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 String name = document.get("name").toString();
                                 Button but = new Button(RoomsList.this);
                                 but.setText(name);
+                                but.setOnClickListener(gameClickEvent);
                                 gamesList.addView(but);
                             }
                         } else {
@@ -45,4 +48,25 @@ public class RoomsList extends AppCompatActivity {
                     }
                 });
     }
+
+    View.OnClickListener gameClickEvent = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Button b = (Button) v;
+            String name = b.getText().toString();
+            Log.d(TAG, name);
+            switch(name){
+                case "Tetris":
+
+                    break;
+                case "Parchís":
+
+                    break;
+                case "Avions":
+
+                    break;
+            }
+        }
+    };
 }
