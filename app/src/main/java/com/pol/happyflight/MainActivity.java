5f7b40bfa50fa7b5d534c06ff39dc3d937ff2f8b
polmina <41476151+polmina@users.noreply.github.com>
2019-10-13 06:24:39 +0200

    duh

--- app/src/main/java/com/pol/happyflight/MainActivity.java
+++ app/src/main/java/com/pol/happyflight/MainActivity.java
@@ -3,18 +3,25 @@ package com.pol.happyflight;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, RoomsList.class);
-        startActivity(intent);
-
-
+        but = findViewById(R.id.button);
+        but.setOnClickListener(list);
     }
+    View.OnClickListener list = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, RoomsList.class);
+            startActivity(intent);
+        }
+    };
 }
