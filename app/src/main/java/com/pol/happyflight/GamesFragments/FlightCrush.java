3a28f7475ff4fff981142dc84656734a9aebfbf6
NIL6NIL6 <niltronera@hotmail.com>
2019-10-13 03:00:41 +0200

    THE ALGORITHM

--- app/src/main/java/com/pol/happyflight/GamesFragments/FlightCrush.java
+++ app/src/main/java/com/pol/happyflight/GamesFragments/FlightCrush.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -28,7 +29,9 @@ import com.google.firebase.firestore.SetOptions;
 import com.pol.happyflight.GameRoom;
 import com.pol.happyflight.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -244,20 +247,207 @@ public class FlightCrush  extends Fragment {
     }
 
     private HashMap<String, Pair<Float, Float>> theAlgorithm(Map<String, Integer> currents, Map<String, Integer> destinations) {
-        HashMap<String, Pair<Float, Float>> deceased = new HashMap<String, Pair<Float, Float>>();
-        HashMap<String, Pair<Float, Float>> lines;
+        HashMap<String, Pair<Float, Float>> res = new HashMap<>();
+
+        //Transform Vectors into Lines
+        HashMap<String, Pair<Float, Float>> lines = getLines(currents, destinations);
+
+        //Change from MAC to id or reverse
+        Pair<HashMap<String,Integer>, ArrayList<String>> conversion = Pair.create(new HashMap<String, Integer>(), new ArrayList<String>());
+        ArrayList<Pair<Float, Float>> lineArray = new ArrayList<Pair<Float, Float>>();
+        ArrayList<Pair<Pair<Float, Float>, Pair<Float, Float>>> vectorArray = new ArrayList<>();
+
+        Iterator it = lines.entrySet().iterator();
+        int i = 0;
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            conversion.first.put(pair.getKey().toString(), i);
+            conversion.second.set(i, pair.getKey().toString());
+            i++;
+
+            //Create Arrays to substitute HashMaps
+            Pair l = lines.get(pair.getKey().toString());
+            lineArray.add(l);
+            Pair v = Pair.create(currents.get(pair.getKey().toString()), destinations.get(pair.getKey().toString()));
+            vectorArray.add(v);
+        }
+
+        //Make lines collide
+        ArrayList<ArrayList<Pair<Pair<Integer, Integer>, Pair<Float,Float>>>> collisionList = collide(lineArray, vectorArray);
+
+        //Sort collisions by how apart they are from their origin
+        ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> sortedList = sortCollisions(collisionList, vectorArray);
+
+        //Destroy all planes that collide
+        HashMap<Integer, Pair<Float, Float>> auxDestroyed = getDestroyedPlanes(sortedList, vectorArray.size());
+        it = auxDestroyed.entrySet().iterator();
+
+        //Change the data structure to the return type
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            res.put(conversion.second.get((int) pair.getKey()), (Pair<Float,Float>) pair.getValue());
+        }
+
+        return res;
+    }
+
+    private HashMap<Integer, Pair<Float, Float>> getDestroyedPlanes(ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> sortedList, int numPlanes) {
+        ArrayList<Boolean> isPlaneDestroyed = new ArrayList<>(Collections.nCopies(numPlanes, false));
+        HashMap<Integer, Pair<Float, Float>> destroyed = new HashMap<>();
+
+        for (int i = 0; i < sortedList.size(); i++) {
+            if (! isPlaneDestroyed.get(sortedList.get(i).first.second) &&
+                ! isPlaneDestroyed.get(sortedList.get(i).first.first)) {
+                
+                isPlaneDestroyed.set(sortedList.get(i).first.first, true);
+                destroyed.put(sortedList.get(i).first.first, sortedList.get(i).second);
+            }
+        }
+
+        return destroyed;
+    }
+
+    private ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> sortCollisions(ArrayList<ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>>> collisionList,
+                                                                                       ArrayList<Pair<Pair<Float, Float>, Pair<Float, Float>>> vectorArray) {
+        //Base cases
+        if (collisionList.size() == 0) {
+            return new ArrayList<>();
+        } else if (collisionList.size() == 1) {
+            return sortPlaneCollisions(collisionList.get(0), vectorArray);
+        }
+
+        //Divide and conquer
+        ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> l1 = sortCollisions((ArrayList) collisionList.subList(0, collisionList.size() / 2), vectorArray);
+        ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> l2 = sortCollisions((ArrayList) collisionList.subList(collisionList.size() / 2, collisionList.size()), vectorArray);
+
+        return mergeCollisions(l1, l2, vectorArray);
+    }
+
+    private ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> sortPlaneCollisions(ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> planeCollisions,
+                                                                                            ArrayList<Pair<Pair<Float, Float>, Pair<Float, Float>>> vectorArray) {
+        int i, j;
+        i = j = 0;
+
+        if (planeCollisions.size() <= 1) {
+            return planeCollisions;
+        }
+
+        ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> l1 = (ArrayList) planeCollisions.subList(0, planeCollisions.size() / 2);
+        ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> l2 = (ArrayList) planeCollisions.subList(planeCollisions.size() / 2, planeCollisions.size());
+
+        return mergeCollisions(l1, l2, vectorArray);
+
+    }
+
+    private ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> mergeCollisions(ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> l1,
+                                                                                        ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> l2,
+                                                                                        ArrayList<Pair<Pair<Float, Float>, Pair<Float, Float>>> vectorArray) {
+        int i, j;
+        i = j = 0;
+
+        ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>> res = new ArrayList<>(l1.size() + l2.size());
+
+        while ((i < l1.size()) && (j < l2.size())) {
+            Float d1, d2;
+            d1 = euclideanDistance(l1.get(i).second, vectorArray.get(l1.get(i).first.first).first);
+            d2 = euclideanDistance(l2.get(j).second, vectorArray.get(l2.get(j).first.first).first);
+            if (d1 < d2) {
+                res.set(i + j, l1.get(i));
+                i++;
+            }
+            else {
+                res.set(i + j, l2.get(j));
+                j++;
+            }
+        }
+
+        while (i < l1.size()) {
+            res.set(i + j, l1.get(i));
+            i++;
+        }
+
+        while (j < l2.size()) {
+            res.set(i + j, l2.get(j));
+            j++;
+        }
+
+        return res;
+    }
+
+    private Float euclideanDistance(Pair<Float, Float> a, Pair<Float, Float> b) {
+        Float x = b.first - a.first;
+        Float y = b.second - a.second;
+        return (float) Math.sqrt(x*x + y*y);
+    }
+
+    private ArrayList< ArrayList< Pair< Pair<Integer, Integer>, Pair< Float, Float> > > > collide(ArrayList<Pair<Float, Float>> lines, ArrayList<Pair<Pair<Float, Float>, Pair<Float, Float>>> vectors) {
+        ArrayList<ArrayList<Pair<Pair<Integer, Integer>, Pair<Float, Float>>>> collisionList = new ArrayList<>(lines.size());
+
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = i + 1; i < lines.size(); j++) {
+                Pair<Float, Float> col = getCollisionPoint(lines.get(i), lines.get(j));
+
+                if (isInVector(col, vectors.get(i)) && isInVector(col,vectors.get(j))) {
+                    collisionList.get(i).add(Pair.create(Pair.create(i, j), col));
+                    collisionList.get(j).add(Pair.create(Pair.create(j, i), col));
+                }
+            }
+        }
+
+        return collisionList;
+    }
+
+    private Boolean isInVector(Pair<Float, Float> point, Pair< Pair<Float, Float>, Pair<Float, Float>> vector) {
+        Float x = (point.first - vector.first.first);
+        Float y = (point.second - vector.first.second);
+        Float xt = (vector.second.first - vector.first.first);
+        Float yt = (vector.second.second - vector.first.second);
+        Float r = ((float) Math.sqrt(x*x + y*y)) / ((float) Math.sqrt(xt*xt + yt*yt));
+        return (r > 0) && (r <= 1);
+    }
+
+    private Pair<Float, Float> getCollisionPoint(Pair<Float, Float> line1, Pair<Float, Float> line2) {
+        /*
+        y = mx + n;
+        y' = m'x' + n';
+        y' = y; x' = x;
+        m'x' + n' = mx + n;
+        (m' - m)x = n - n';
+
+        x = (n - n') / (m' - m)
+         */
+
+        Float x = (line2.second - line1.second) / (line1.first - line2.first);
+
+        // y = m*x + n
+        Float y = line1.first * x + line1.second;
+
+        return Pair.create(x,y);
+    }
+
+    private HashMap<String, Pair<Float, Float>> getLines(Map<String, Integer> currents, Map<String, Integer> destinations) {
+        HashMap<String, Pair<Float, Float>> lines = new HashMap<String, Pair<Float, Float>>();
 
         Iterator it = currents.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry) it.next();
 
-            String d1 = "destination".concat(Integer.toString(currents.get(pair.getKey())));
-            int resID = getResources().getIdentifier(buttonID, "id", "com.pol.happyflight");
-            Button b1 = (Button) view.findViewById(d1);
+            //Current position's button (Need to get its position)
+            String buttonID1 = "destination".concat(Integer.toString(currents.get(pair.getKey())));
+            int resID1 = getResources().getIdentifier(buttonID1, "id", "com.pol.happyflight");
+            ImageButton b1 = (ImageButton) view.findViewById(resID1);
 
-            Float slope;
+            //Destination's button (Need to get its position)
+            String buttonID2 = "destination".concat(Integer.toString(destinations.get(pair.getKey())));
+            int resID2 = getResources().getIdentifier(buttonID2, "id", "com.pol.happyflight");
+            ImageButton b2 = (ImageButton) view.findViewById(resID2);
+
+            Float slope = (b2.getY() - b1.getY()) / (b2.getX() - b1.getX());
+            Float axisCut = b2.getY() - slope * b2.getX();
+
+            lines.put((String) pair.getKey(), Pair.create(slope, axisCut));
         }
 
-        return deceased;
+        return lines;
     }
 }
